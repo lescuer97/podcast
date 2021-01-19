cfff5b193dac0bb4ae043a0ccca8bd53c943171b
Leonardo Escuer <leoescuer@protonmail.com>
2021-01-19 11:36:57 +0100

    added linter
    Signed-off-by: Leonardo Escuer <leoescuer@protonmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
- import "tailwindcss/tailwind.css";
+import "tailwindcss/tailwind.css";
 
-import { AppProps } from 'next/app'
+import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
