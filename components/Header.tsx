47a79c7477c5602736d859fc22b9b3bf2a0ff47f
lescuer97 <leoescuer@protonmail.com>
2021-01-21 14:43:37 +0100

    added url
    Signed-off-by: lescuer97 <leoescuer@protonmail.com>

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Link from 'next/link';
 
 const Header = () => {
+
+
   return (
     <header className="flex flex-col justify-between md:flex-row md:w-4/5 my-3">
-      <div>Leito's Complaints</div>
+      <Link href="/" ><a>Leito's Complaints</a></Link>
       <ul className="flex flex-col md:flex-row space-y-1 md:space-y-0 md:space-x-5">
         <li className="">Itunes</li>
         <li className="">Spotify</li>
