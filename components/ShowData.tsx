2cc2489ae50bb13331160de2cae8de6dfd00c4c9
lescuer97 <leoescuer@protonmail.com>
2021-01-24 22:52:39 +0100

    sticky notes when scrolling above 798px
    Signed-off-by: lescuer97 <leoescuer@protonmail.com>

--- components/ShowData.tsx
+++ components/ShowData.tsx
@@ -1,4 +1,5 @@
 const ShowData = (props) => {
+  // TODO MAKE SHOW NOTES STIKY TO THE TOP SO ONLY THE ESPISODES SCROLL 
   return (
     <div className="scroll" dangerouslySetInnerHTML={{ __html: props.dat }} />
   );
