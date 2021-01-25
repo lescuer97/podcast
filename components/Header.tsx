de980bc92875cee84cd7ee44eeac2c6cc3109a06
lescuer97 <leoescuer@protonmail.com>
2021-01-25 06:57:27 +0100

    adding css
    Signed-off-by: lescuer97 <leoescuer@protonmail.com>

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,9 @@ const Header = () => {
 
 
   return (
-    <header className="flex flex-col justify-between md:flex-row md:w-4/5 my-3">
+    <header className="flex flex-col md:justify-between md:header-height md:flex-row w-full px-4 ">
       <Link href="/" ><a>Leito's Complaints</a></Link>
-      <ul className="flex flex-col md:flex-row space-y-1 md:space-y-0 md:space-x-5">
+      <ul className="flex flex-wrap md:flex-row space-y-1 md:space-y-0 md:space-x-5">
         <li className="">Itunes</li>
         <li className="">Spotify</li>
         <li className="">Google Play</li>
