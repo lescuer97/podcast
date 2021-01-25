4161c113a480540f933eada84463cb8a82821fa0
lescuer97 <leoescuer@protonmail.com>
2021-01-25 20:22:39 +0100

    changed to normal function and added id
    Signed-off-by: lescuer97 <leoescuer@protonmail.com>

--- components/ShowData.tsx
+++ components/ShowData.tsx
@@ -1,8 +1,9 @@
-const ShowData = (props) => {
+
+export default function ShowData (props)  {
   // TODO MAKE SHOW NOTES STIKY TO THE TOP SO ONLY THE ESPISODES SCROLL 
   return (
-    <div className="scroll" dangerouslySetInnerHTML={{ __html: props.dat }} />
+    <div  className="scroll" id="scroll" dangerouslySetInnerHTML={{ __html: props.dat }} />
   );
 };
 
-export default ShowData;
+
