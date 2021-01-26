cb64ce9c023fc1f6b1d500fb7ec05bc11cf740ef
lescuer97 <leoescuer@protonmail.com>
2021-01-26 15:37:19 +0100

    fixing the header and imports
    Signed-off-by: lescuer97 <leoescuer@protonmail.com>

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 import Link from 'next/link';
-import Play from "./svgs/Play";
-import Youtube from "./svgs/Youtube";
-import Spotify from "./svgs/Spotify";
-import Itunes from "./svgs/Itunes";
+
+import {Youtube, Spotify, Itunes, Play} from "./svgs/small_logos";
+
+import Logo from "./svgs/Logo";
 
 export default function Header()  {
 
-
   return (
     <header className="header justify-center" id="header"> 
-      <Link href="/" ><a className="p-3">Leito's Complaints</a></Link>
-      <ul className="flex flex-row md:flex-row  space-y-1 md:space-y-0 md:space-x-5  ">
+      <Link href="/" ><a className=""><Logo /></a></Link>
+      <ul className="flex flex-row md:flex-row   md:mt-2 ">
         <div className=" flex flex-col md:flex-row justify-center">
-          <li className="px-4 py-2 mx-2   text-center "> <Itunes style="svg inline"/> Itunes</li>
-          <li className="px-4 py-2 mx-2  text-center"> <Spotify style="svg fill-current inline"/> Spotify</li>
+          <li className="px-4 py-2 mx-2   text-center "> 
+            <Itunes /> Itunes</li>
+          <li className="px-4 py-2 mx-2  text-center"> 
+            <Spotify /> Spotify</li>
         </div>
         <div className="flex flex-col md:flex-row justify-center">
           <li className="px-4 py-2 mx-2  text-center"> 
-               <Play style="svg inline" /> Google Play</li>
+               <Play  /> Google Play</li>
           <li className="px-4 py-2 mx-2  text-center"> 
-              <Youtube style="svg inline"/> Youtube</li>
+              <Youtube /> Youtube</li>
         </div>
       </ul>
     </header>
