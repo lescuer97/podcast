e9ee439506547aa4cb2a1dd5eb0dd07d179670be
lescuer97 <leoescuer@protonmail.com>
2021-01-26 16:47:14 +0100

    aaded correct favicon and removed comment
    Signed-off-by: lescuer97 <leoescuer@protonmail.com>

--- components/ShowData.tsx
+++ components/ShowData.tsx
@@ -1,8 +1,8 @@
 
 export default function ShowData (props)  {
-  // TODO MAKE SHOW NOTES STIKY TO THE TOP SO ONLY THE ESPISODES SCROLL 
+
   return (
-    <div  className="scroll" id="scroll" dangerouslySetInnerHTML={{ __html: props.dat }} />
+    <div  className="scroll" id="scroll" dangerouslySetInnerHTML={{ __html: props.list }} />
   );
 };
 
